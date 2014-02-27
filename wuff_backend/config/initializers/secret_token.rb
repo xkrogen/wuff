a209386c3ba93a04d9253982d054d54bcef19488
Erik <erikkrogen@gmail.com>
2014-02-26 17:55:33 -0800

    Initialize Rails server

---
+++ wuff_backend/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WuffBackend::Application.config.secret_key_base = 'fd297fd240011365b5da1836058b36a8c60b622eee14eac045c14564596f296d40446c3965371a504352cf981cc1aa28816cb9a979b72497b5ad506bf9be4f31'
