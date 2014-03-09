665eac9e6e25f17543651837f10ae895cf276f12
Matt Griffin <mattgriffin94@yahoo.com>
2014-03-09 15:09:44 -0700

    Added initWithCoder method to input View
    Makes it so you don't need to force an initWithFrame from the
    viewController if you already made it in storyboard

--- wuff_frontend/wuff_frontend/LoginViewController.m
+++ wuff_frontend/wuff_frontend/LoginViewController.m
@@ -18,7 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    [_emailInputView initWithFrame:[[UIScreen mainScreen] bounds]];
 }
 
 - (void)didReceiveMemoryWarning
