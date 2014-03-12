f7157bd9bd93e5c63ab96d08eb501ecaee1e5a8c
Matt Griffin <mattgriffin94@yahoo.com>
2014-03-11 23:31:52 -0700

    Event create view Controller

---
+++ wuff_frontend/wuff_frontend/EventCreateViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventCreateViewController.h
+//  wuff_frontend
+//
+//  Created by Matthew Griffin on 3/11/14.
+//  Copyright (c) 2014 Wuff Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventCreateViewController : UIViewController
+
+@end
